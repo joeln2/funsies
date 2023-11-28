104d99b8aba53b115e081c3e3edeef2e3848bc1f
joeln2 <129252192+joeln2@users.noreply.github.com>
2023-11-28 20:12:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,17 +13,26 @@ yesBtn.addEventListener("click", () => {
     gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
     btnGroup.style.display = "none"; // Hides the button group
     buttonsDisabled = true; // Disables button functionality
-  }
-});
 
-noBtn.addEventListener("click", () => {
-  if (!buttonsDisabled) {
+    // Displace the "No" button immediately
     const noBtnRect = noBtn.getBoundingClientRect();
     const maxX = window.innerWidth - noBtnRect.width;
     const maxY = window.innerHeight - noBtnRect.height;
 
     const randomX = Math.floor(Math.random() * maxX);
     const randomY = Math.floor(Math.random() * maxY);
+
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomY + "px";
+  }
+});
+
+noBtn.addEventListener("click", () => {
+  // No functionality after the "Yes" button is clicked
+  if (!buttonsDisabled) {
+    // Placeholder code or any functionality you might want
+  }
+});
 
     noBtn.style.left = randomX + "px";
     noBtn.style.top = randomY + "px";
