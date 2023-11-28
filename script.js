be3037118accbb8093ea991050af4947c407037b
joeln2 <129252192+joeln2@users.noreply.github.com>
2023-11-28 20:00:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,21 +3,10 @@ const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
+const btnGroup = document.querySelector(".btn-group"); // Selecting the container for the buttons
 
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "Yay, see you on the 18th!";
-  gif.src =
-    "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
-});
-
-noBtn.addEventListener("mouseover", () => {
-  const noBtnRect = noBtn.getBoundingClientRect();
-  const maxX = window.innerWidth - noBtnRect.width;
-  const maxY = window.innerHeight - noBtnRect.height;
-
-  const randomX = Math.floor(Math.random() * maxX);
-  const randomY = Math.floor(Math.random() * maxY);
-
-  noBtn.style.left = randomX + "px";
-  noBtn.style.top = randomY + "px";
+  gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+  btnGroup.style.display = "none"; // Hides the button group
 });
