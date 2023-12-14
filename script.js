749d63977ea3c88e649c680b2b484b361a13546a
joeln2 <129252192+joeln2@users.noreply.github.com>
2023-12-14 23:08:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,24 @@
-const wrapper = document.querySelector(".wrapper");
-const question = document.querySelector(".question");
-const gif = document.querySelector(".gif");
-const yesBtn = document.querySelector(".yes-btn");
-const noBtn = document.querySelector(".no-btn");
-const btnGroup = document.querySelector(".btn-group"); // Selecting the container for the buttons
+const wrapper = document.querySelector('.wrapper');
+const question = document.querySelector('.question');
+const gif = document.querySelector('.gif');
+const yesBtn = document.querySelector('.yes-btn');
+const noBtn = document.querySelector('.no-btn');
 
-let buttonsDisabled = false; // Flag to keep track of button disablement
-
-yesBtn.addEventListener("click", () => {
-  if (!buttonsDisabled) {
-    question.innerHTML = "LESSGOO see you at 7 at rsi ;)";
-    gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
-    btnGroup.style.display = "none"; // Hides the button group
-    buttonsDisabled = true; // Disables button functionality
-
-    // Displace the "No" button immediately
-    const noBtnRect = noBtn.getBoundingClientRect();
-    const maxX = window.innerWidth - noBtnRect.width;
-    const maxY = window.innerHeight - noBtnRect.height;
-
-    const randomX = Math.floor(Math.random() * maxX);
-    const randomY = Math.floor(Math.random() * maxY);
-
-    noBtn.style.left = randomX + "px";
-    noBtn.style.top = randomY + "px";
-  }
+yesBtn.addEventListener('click', () => {
+  question.innerHTML = 'AWWWW i knew you loved me sethu. n e ways see you on new years';
+  gif.src = 'https://media.tenor.com/VBKt3ZgNpzsAAAAM/chica-fnaf.gif';
+  yesBtn.style.display = 'none';
+  noBtn.style.display = 'none';
 });
 
-noBtn.addEventListener("click", () => {
-  // No functionality after the "Yes" button is clicked
-  if (!buttonsDisabled) {
-    // Placeholder code or any functionality you might want
-  }
-});
+noBtn.addEventListener('mouseover', () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
 
-    noBtn.style.left = randomX + "px";
-    noBtn.style.top = randomY + "px";
-  }
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + 'px';
+  noBtn.style.top = randomY + 'px';
 });
